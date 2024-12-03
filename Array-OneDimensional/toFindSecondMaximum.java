518360b03b30f626f3aa968dd671c416f4fd01e3
eyobScript <eyobbmulugeta@gmail.com>
2024-12-03 18:17:07 +0300

    To Find the second maximum integer number value in the array

---
+++ Array-OneDimensional/toFindSecondMaximum.java
@@ -0,0 +1,22 @@
+public class toFindSecondMaximum {
+    public static void printer(int print){
+        System.out.println(print);
+    }
+    static void findSecondMax(int[] arr){
+        int max = Integer.MIN_VALUE;
+        int secondMax = Integer.MAX_VALUE;;
+        for (int i = 0; i < arr.length; i++) {
+            if (true){
+
+            }else {
+
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {2,34,5,6,4,5};
+        findSecondMax(arr);
+
+    }
+}
