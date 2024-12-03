2620d02064784e25894ecb4d532c9828b33667c9
eyobScript <eyobbmulugeta@gmail.com>
2024-12-03 15:25:37 +0300

    Find a leaders

---
+++ Array-OneDimensional/FindLeaders.java
@@ -0,0 +1,19 @@
+public class FindLeaders {
+//    Given an array A of positive integers.
+//    Your task is to find the leaders in the array.
+//    An element of array is leader if it is greater than or equal to all the elements to its right side.
+//    The rightmost element is always a leader
+
+    public static void printer(int[] arr) {
+        for (int a : arr){
+            System.out.print( a + " ");
+        }
+        System.out.println();
+    }
+    public static void findLeaders(int[] arr){
+
+    }
+    public static void main(String[] args) {
+
+    }
+}
