20552101010ebd7e0efc7c6bebf2d8a62ea4ace5
eyobScript <eyobbmulugeta@gmail.com>
2025-02-21 18:52:54 +0300

    217. Contains Duplicate
    Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.

--- Algorithms/Python/Problems/Contains_Duplicate.py
+++ Algorithms/Python/Problems/Contains_Duplicate.py
@@ -1,3 +1,12 @@
+"""
+217. Contains Duplicate
+
+Given an integer array nums, return true if any value appears at least twice in the array,
+and return false if every element is distinct.
+
+"""
+
+
 def containsDuplicate(nums):
     return len([n for n in set(nums)]) != len(nums)
 resul = containsDuplicate([1,2,3,1])
